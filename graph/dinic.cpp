0d083b832883ff602825a29f12a0025719dbd2c4
EntropyIncreaser <2743134282@qq.com>
2019-10-27 23:03:57 +0800

    add dinic template

---
+++ graph/dinic.cpp
@@ -0,0 +1,73 @@
+template <class Z>
+struct MaxFlow {
+  vector<vector<tuple<int, int, Z>>> g;
+
+  struct Ref {
+    const MaxFlow* p;
+    int u, id;
+
+    Ref() {}
+
+    Ref(const MaxFlow* p, int u, int id) : p(p), u(u), id(id) {}
+
+    Z get() const { return std::get<2>((p->g)[u][id]); }
+  };
+
+  MaxFlow() {}
+  MaxFlow(int n) : g(n) {}
+
+  Ref addEdge(int u, int v, const Z& w) {
+    if (u == v) return Ref();
+    int ru = g[u].size(), rv = g[v].size();
+    g[u].emplace_back(v, rv, w);
+    g[v].emplace_back(u, ru, 0);
+    return Ref(this, v, rv);
+  }
+
+  Z dinic(int s, int t) {
+    int n = g.size();
+    function<vector<int>()> getLevel = [&]() {
+      vector<int> level(n, -1);
+      queue<int> q;
+      level[s] = 0;
+      q.push(s);
+      while (!q.empty()) {
+        int u = q.front(); q.pop();
+        for (const auto& pr : g[u])
+          if (get<2>(pr) && level[get<0>(pr)] == -1) {
+            level[get<0>(pr)] = level[u] + 1;
+            q.push(get<0>(pr));
+          }
+      }
+      return level;
+    };
+    vector<int> r, level;
+    function<Z(int, Z)> cap = [&](int u, Z limit) {
+      if (u == t) return limit;
+      Z ret = 0;
+      while (r[u] && limit > ret) {
+        int v = get<0>(g[u][r[u] - 1]), rev = get<1>(g[u][r[u] - 1]);
+        Z& w = get<2>(g[u][r[u] - 1]);
+        if (level[v] == level[u] + 1) {
+          Z flow = cap(v, min(limit - ret, w));
+          ret += flow;
+          w -= flow;
+          get<2>(g[v][rev]) += flow;
+          if (limit == ret)
+            return ret;
+          --r[u];
+        } else
+          --r[u];
+      }
+      return ret;
+    };
+    Z ret = 0;
+    while (level = getLevel(), level[t] != -1) {
+      r = vector<int>(n);
+      for (int i = 0; i < n; ++i)
+        r[i] = g[i].size();
+      ret += cap(s, numeric_limits<Z>::max());
+    }
+    return ret;
+  }
+};
